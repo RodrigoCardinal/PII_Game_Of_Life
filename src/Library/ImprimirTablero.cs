7b3a97165fb6f63b40a28e445c62064b35f651e6
Prestamo <Prestamo@P-CND7012TDS.ucuadm.ucu.edu.uy>
2023-09-05 17:17:06 -0300

    sin terminar

---
+++ src/Library/ImprimirTablero.cs
@@ -0,0 +1,38 @@
+using System;
+namespace PII_Game_Of_Life
+{
+    public class ImprimirTablero
+    {
+        public void Print(bool[,] b)
+        {
+            //bool[,] b //variable que representa el tablero
+            int width = b.GetLength(0); //variabe que representa el ancho del tablero
+            int height = b.GetLength(1); //variabe que representa altura del tablero
+       
+            Console.Clear();
+            StringBuilder s = new StringBuilder();
+            for (int y = 0; y<height;y++)
+            {
+                for (int x = 0; x<width; x++)
+                {
+                    if(b[x,y])
+                    {
+                        s.Append("|X|");
+                    }
+                    else
+                    {
+                        s.Append("___");
+                    }
+                }
+                s.Append("\n");
+            }
+            Console.WriteLine(s.ToString());
+            //=================================================
+            //Invocar método para calcular siguiente generación
+            //=================================================
+            Thread.Sleep(300);
+        }
+    }
+}
+
+
